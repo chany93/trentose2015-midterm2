60b7f7f6b895be56a5676c0e5483cb8fa14226f5
chany93 <giovannirafael.vuolo@studenti.unitn.it>
2015-11-05 10:32:24 +0100

    messaggio

--- app.js
+++ app.js
@@ -14,9 +14,38 @@ var tmpl = ' <li id="ID">' +
            '  <h3 class="solution">SOLUTION</h3>'+
            ' </li> ';
 
+var tmpl_2 =  '<li id="1" class="current">' +
+                ' <h3 class="domanda">inglese</h3>' +
+         '<h3 class="solution">tedesco</h3> ' +  
+              '</li>';                       
+    
+               
+  var correct = 0;            
+var i = 0;
+
 $(document).ready(function(){
-
-
+     
+    //while (i < data.length){
+        $('.solution').hide();                                                      // volevo fare tutto dentro 
+        var add_to_ul = tmpl_2.replace("inglese", data[i].word_en)
+                            .replace("tedesco", data[i].word_de);
+        $(".cards").append(add_to_ul);
+        $('.solution').hide();
+        $('.current').on('click', function(){
+            i++;
+        $('.domanda').hide();
+        $('.solution').show();
+    })
+    //        var add_to_ul = tmpl_2.replace("inglese", data[i].word_en)
+    //                            .replace("tedesco", data[i].word_de);
+    //        $(".cards").append(add_to_ul);
+    
+    //}
+     $('.options').on('click', function(){
+        if (("this .btn opt-correct")){
+            correct++;
+        }
+     })
 });
 
 
